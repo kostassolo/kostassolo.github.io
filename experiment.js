80ba07ff25fde1e5d232ae7d8c7989772c4fe2cf
kostassolo <ksolom6@uic.edu>
2023-08-30 13:38:56 -0500

    test

--- experiment.js
+++ experiment.js
@@ -7,7 +7,7 @@ class Rep {
     let bb = await sharedStorage.remainingBudget();
     console.log(typeof storedData);
     console.log(bb)
-    return 1234
+    return storedData
    }
 }
 register('report', Rep);
