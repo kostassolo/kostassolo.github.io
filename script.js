3ea822ee3b43e7090e9e69e53480e8edc318641d
kostassolo <ksolom6@uic.edu>
2024-01-16 14:10:14 -0600

    addition

---
+++ script.js
@@ -0,0 +1,8 @@
+// external-script.js
+document.addEventListener('DOMContentLoaded', function () {
+  // Your code to register an iframe in the app
+  const iframe = document.createElement('iframe');
+  iframe.src = 'https://example.com';
+  document.body.appendChild(iframe);
+});
+
