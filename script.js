ca6264a52b9ad7a6d87a469ed704c6a5c1b51ee3
kostassolo <ksolom6@uic.edu>
2024-01-16 19:45:46 -0600

    test

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Your code to register an iframe in the app
   const iframe = document.createElement('iframe');
-  iframe.src = 'https://example.com';
+  iframe.src = '/Users/kostassolomos/Desktop/electron/csp_testing_scriptsrc/external.html';
   document.body.appendChild(iframe);
 });
 
