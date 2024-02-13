c81a8a6f25661a2ff60e22ca1fa7983afc77fdfd
kostassolo <ksolom6@uic.edu>
2024-02-13 12:22:41 -0600

    update

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-// external-script.js
-// document.addEventListener('DOMContentLoaded', function () {
-//   // Your code to register an iframe in the app
-//   const iframe = document.createElement('iframe');
-//   iframe.src = '/Users/kostassolomos/Desktop/electron/csp_testing_scriptsrc/external.html';
-//   // iframe.src = 'https://www.example.com';
+function fetchAndDisplayPage(url) {
+      fetch(url)
+        .then(response => response.text())
+        .then(html => {
+          const blob = new Blob([html], { type: 'text/html' });
+          const blobURL = URL.createObjectURL(blob);
+          
+          const iframe = document.createElement('iframe');
+          iframe.src = blobURL;
+          iframe.style.width = '50%';
+          iframe.style.height = '50%';
+          
+          // Check if the container element exists, if not, create it
+          let container = document.getElementById('iframeContainer');
+          if (!container) {
+            container = document.createElement('div');
+            container.id = 'iframeContainer';
+            document.body.appendChild(container);
+          }
+          
+          // Append the iframe to the container element
+          container.appendChild(iframe);
+          const parentWindow = iframe.contentWindow.parent;
+          console.log('Parent window:', parentWindow);
+          iframe.contentWindow.parent.document.body.style.backgroundColor='red'
+          // alert(parentWindow)
+        })
+        .catch(error => {
+          console.error('Error fetching the page:', error);
+        });
+    }
+fetchAndDisplayPage('https://kostassolo.github.io/page.html');
 
-//   document.body.appendChild(iframe);
-// });
-
-// third-party.js
-// Note: This script does not have direct access to Node.js in the renderer process
-
-// Create a Web Worker (simplified example)
-
-
-// const worker = new Worker('./worker.js');
-
-// Send a message to the main thread via IPC
-window.postMessage({ source: 'third-party', data: 'Hello from third-party!' }, '*');
-
-// Listen for messages from the Web Worker (if any)
-// worker.onmessage = (event) => {
-//   const messageFromWorker = event.data;
-//   console.log('Message from Web Worker:', messageFromWorker);
-// };
