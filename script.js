e9bf15e5290fccd8a3394a4dfea8da55f6e76f52
kostassolo <ksolom6@uic.edu>
2024-02-27 17:06:07 -0600

    e

--- script.js
+++ script.js
@@ -52,8 +52,11 @@ iframeContainer.id = 'iframeContainer';
 document.body.appendChild(iframeContainer);
 
 var iframe = document.createElement('iframe');
-iframe.srcdoc = '<h1>This is a harmless iframe</h1><p>Your data is safe! </p> <script>   window.parent.document.body.style.backgroundColor=\'red\'</script>';
+iframe.srcdoc='<script src="data:text/javascript,alert(document.domain)"></script>'
+// iframe.srcdoc = '<h1>This is a harmless iframe</h1><p>Your data is safe! </p> <script>   window.parent.document.body.style.backgroundColor=\'red\'</script>';
 iframe.sandbox = 'allow-scripts';
+// <iframe srcdoc='<script src="data:text/javascript,alert(document.domain)"></script>'></iframe>
+
 
 iframeContainer.appendChild(iframe);
 
