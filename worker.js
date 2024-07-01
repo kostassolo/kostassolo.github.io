699a6be101f443fdb359b244b395af46b40ce5a5
kostassolo <ksolom6@uic.edu>
2024-07-01 15:37:57 -0500

    e

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 // This would be on the third-party domain (example.com)
 self.onmessage = function(event) {
     console.log('Third-Party Worker received:', event.data);
-    self.postMessage('Hello from Third-Party Worker');
+    self.postMessage('Hello from ');
 };
