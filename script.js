0ce74a2fb461b1e18fd01bb34b11655c16f93c01
kostassolo <ksolom6@uic.edu>
2024-07-10 11:51:23 -0500

    script

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Create the iframe element
-    var iframe = document.createElement('iframe');
-    // iframe.sandbox = ''
-    iframe.srcdoc = `
-        <html>
-            <head>
-                <script>
-                    // Log the iframe's cookies to the console
-                    console.log('Iframe from github read cookies: ' + document.cookie);
-                    // Log the iframe's origin to the console
-                    console.log('Iframe  from github read origin: ' + document.domain);
-                <\/script>
-            </head>
-            <body>
-                <h2>Iframe Content</h2>
-            </body>
-        </html>`;
-    
-    // Append the iframe to the body
-    document.body.appendChild(iframe);
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('/var/log/fsck_hfs.log')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('logContent').innerText = data;
+        })
+        .catch(error => {
+            document.getElementById('logContent').innerText = `Fetch error: ${error}`;
+        });
 });
