3a9d56555e0cc5883f4aeb4d3119824c96eaac8b
kostassolo <ksolom6@uic.edu>
2024-07-10 12:32:54 -0500

    script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('/var/log/fsck_hfs.log')
+    fetch('/var/log/wifi.log')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
